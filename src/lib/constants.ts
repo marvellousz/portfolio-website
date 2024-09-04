fd6bd0bef35ad8b2039320997fd84e8cba31d814
Pranav <pranavmurali024@gmail.com>
2024-09-04 11:39:12 +0530

    updated resume

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const LINKS = {
   RESUME:
-    "https://drive.google.com/file/d/1NdAdRxjf3jWsUrCAyzlSEX-jgwrSlzjt/view?usp=sharing",
+    "https://drive.google.com/file/d/1JGKDKG3YH4fg_vmvFLWrwHYosNBDIC4T/view?usp=sharing",
 };
