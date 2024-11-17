b102064dc67f4e33d8c5bbda842acfaf059f9fa0
Marvellousz <pranavmurali024@gmail.com>
2024-11-17 14:38:44 +0530

    changed resume

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const LINKS = {
   RESUME:
-    "https://drive.google.com/file/d/1SLZJ_stLi5o07tWYW5ilVDaM_eBdVlwF/view?usp=sharing",
+    "https://drive.google.com/file/d/1x5IFTKlT0J75L2kX5jVFWKw_lrIdH1Ea/view?usp=sharing",
 };
